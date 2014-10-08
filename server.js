3a0681c4dadad946bcd17d7f22b350a2cafde24a
Will Anderson <will@itsananderson.com>
2014-10-08 00:35:47 -0700

    Use more robust body parsing method, and try to parse body as JSON

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var http = require('http'),
-    storage = require('azure-storage');
+    storage = require('azure-storage'),
+    rawBody = require('raw-body'),
+    typer = require('media-typer');
 
 var account = process.env.RECORD_PAYLOAD_ACCOUNT;
 var key = process.env.RECORD_PAYLOAD_KEY;
 var containerName = process.env.RECORD_PAYLOAD_CONTAINER || 'payload-records';
+var maxBodySize = process.env.RECORD_PAYLOAD_MAX_BODY_SIZE || '1mb';
 
 var blobService = storage.createBlobService(account, key);
-
-console.log(String(new Date().getTime()));
 
 blobService.createContainerIfNotExists(containerName, function(err, result, response) {
     if (err) {
@@ -16,12 +17,30 @@ blobService.createContainerIfNotExists(containerName, function(err, result, resp
     } else {
         http.createServer(function(req, res) {
             var headers = req.headers;
-            var body = '';
-            req.on('data', function(data) {
-                body += data.toString('utf8');
-            });
-            req.on('end', function() {
-                var payload = JSON.stringify({ headers: headers, body: body}, undefined, 2);
+            rawBody(req, {
+                limit: maxBodySize,
+                encoding: req.headers['content-type'] ? typer.parse(req.headers['content-type']).parameters.charset : 'utf-8'
+            }, function(err, body) {
+                if (err) {
+                    res.end(err.toString());
+                }
+
+                var bodyStr = body.toString('utf8');
+                var bodyObj;
+
+                try {
+                    bodyObj = JSON.parse(bodyStr);
+                } catch (e) {
+                    bodyObj = bodyStr;
+                }
+
+                var payload = JSON.stringify({
+                    url: req.url,
+                    method: req.method,
+                    headers: headers,
+                    body: bodyObj
+                }, undefined, 2);
+
                 blobService.createBlockBlobFromText(
                     containerName,
                     String(new Date().getTime()),
@@ -31,9 +50,11 @@ blobService.createContainerIfNotExists(containerName, function(err, result, resp
                         if(error){
                             console.log("Couldn't upload payload");
                             console.error(error);
+                            res.setHeader('Content-Type', 'text/plain');
                             res.end(error);
                         } else {
                             console.log('Payload uploaded successfully');
+                            res.setHeader('Content-Type', 'text/plain');
                             res.end('success');
                         }
                     });
